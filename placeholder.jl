f9f75a5eba941cd44770107d7e2bb1cb017ea0e5
Martinho Hoffman <39743428+martinhohoff@users.noreply.github.com>
2022-05-30 12:31:29 -0300

    Create placeholder.jl

---
+++ placeholder.jl
@@ -0,0 +1,4 @@
+{"description":"\"Sunshine bag made of leather and plexiglass handles. Lined internal compartment, gold-finish metalware. Made in Italy\"","manufacturer_variant_url":"https://www.brand.com/bags/model1","":""}
+{"description":"\"Medium bag","manufacturer_variant_url":"beige leather with zip fastening.","":""}
+{"description":"Spacious interior","manufacturer_variant_url":"zip pocket metalware.","":""}
+{"description":"Adjustable","manufacturer_variant_url":"shoulder strap.\"","":"https://www.brand.com/bags/model2"}
