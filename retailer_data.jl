edefc9dea974aff2dacfbaa8616c1656d8bcc66f
Martinho Hoffman <39743428+martinhohoff@users.noreply.github.com>
2022-12-23 18:31:02 -0300

    Update retailer_data.jl

--- retailer_data.jl
+++ retailer_data.jl
@@ -1,2 +1,2 @@
-{"description":"Sunshine bag made of leather and plexiglass handles. Lined internal compartment, gold-finish metalware. Made in Italy","manufacturer_variant_url":"https://www.brand.com/bags/model1"}
-{"description":"Medium bag, beige leather with zip fastening.\nSpacious interior, zip pocket metalware.\nAdjustable, shoulder strap.","manufacturer_variant_url":"https://www.brand.com/bags/model2"}
+{"contract_number": "abcde", "title": "Software Licenses"}
+{"contract_number": "defg", , "title": "Purchasing Equipment"}
